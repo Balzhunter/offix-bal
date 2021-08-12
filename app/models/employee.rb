080a864d5366920bd3c08d3b6c8a29fd59afe531
balzhunter <balvirp1@gmail.com>
2021-08-12 16:21:29 +0000

    Add employees validations

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,17 @@
 class Employee < ApplicationRecord
+  # Validations
+  validates :name, presence: true, uniqueness: true
+  validates :birth_date, presence: true
+  validate :minimum_age
+
+  # Associations
   belongs_to :department
+
+  private
+
+  def minimum_age
+    return if birth_date <= 18.years.ago
+
+    errors.add(:birth_date, "should be before 18 years ago")
+  end
 end
